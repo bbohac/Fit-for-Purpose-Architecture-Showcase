e2a154ac11bd52530f315e90657d91c0018f9230
Bastian Bohac <bastian.bohac@gmail.com>
2026-01-14 14:47:16 +0100

    feat: add domain entity for URL shortener

---
+++ 02-layered-architecture/src/Domain/Entities/ShortUrl.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities;
+
+public sealed class ShortUrl
+{
+    public ShortUrl(string code, string originalUrl)
+    {
+        Code = code;
+        OriginalUrl = originalUrl;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public string Code { get; }
+
+    public string OriginalUrl { get; }
+
+    public DateTime CreatedAt { get; }
+}
