35152ac31b5952a0f7d4affd15eb0621fb60e196
Bastian Bohac <bastian.bohac@gmail.com>
2026-01-14 14:53:35 +0100

    feat: add application service for URL shortening

---
+++ 02-layered-architecture/src/Application/Services/ShorteningService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public sealed class ShorteningService
+{
+    private readonly IUrlRepository _repository;
+
+    public ShorteningService(IUrlRepository repository) => _repository = repository;
+
+    public ShortUrl Shorten(string url)
+    {
+        if (
+            !Uri.TryCreate(url, UriKind.Absolute, out var uri)
+            || (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps)
+        )
+        {
+            throw new ArgumentException("Invalid URL format.", nameof(url));
+        }
+
+        var code = _repository.Add(url);
+        return new ShortUrl(code, url);
+    }
+
+    public string? Resolve(string code) =>
+        string.IsNullOrWhiteSpace(code) ? null : _repository.Get(code);
+}
